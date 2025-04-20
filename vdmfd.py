0f6faa998cb8d5e9bd6e89e26eae52c4eb40551a
Neuro-NX <NeuroNX.dev@proton.me>
2025-04-20 14:48:07 +0100

    feat: add function to colorize text

--- vdmfd.py
+++ vdmfd.py
@@ -28,6 +28,26 @@ import subprocess
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
+class Colors:
+    # FIXME: How to both color text and make it bold?
+    def __init__(self):
+        self.drk_yellow = '\033[33m'
+        self.red = '\033[91m'
+        self.green = '\033[92m'
+        self.blue = '\033[94m'
+        self.yellow = '\033[93m'
+        self.magenta = '\033[95m'
+        self.cyan = '\033[96m'
+        self.white = '\033[97m'
+        self.bold = '\033[1m'
+        self.underline = '\033[4m'
+        self.reset = '\033[0m'
+    def colorize(self, text, color):
+        color_code = getattr(self, color.lower(), '')
+        return f"{color_code}{text}{self.reset}"
+
+colors = Colors()
+
 # Map of unit multipliers for file size.
 SIZE_UNITS = {
     "b": 1,
